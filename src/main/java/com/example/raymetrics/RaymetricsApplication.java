d93992dc517cfd3487d5d4e91c58d740aeca0285
eunji <cije0504@naver.com>
2023-04-12 02:22:12 +0900

    원복

--- src/main/java/com/example/raymetrics/RaymetricsApplication.java
+++ src/main/java/com/example/raymetrics/RaymetricsApplication.java
@@ -2,17 +2,11 @@ package com.example.raymetrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
-public class RaymetricsApplication extends SpringBootServletInitializer {
+public class RaymetricsApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-        return builder.sources(RaymetricsApplication.class);
-    }
     public static void main(String[] args) {
         SpringApplication.run(RaymetricsApplication.class, args);
     }
